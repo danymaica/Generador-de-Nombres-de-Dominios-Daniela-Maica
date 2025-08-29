c1cbd4c71b5a69beb6b4c9e3b084565bf1b162d4
Daniela Maica <danielamaica43@gmail.com>
2025-08-29 03:03:14 +0000

    Generador

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 import "bootstrap";
 import "./style.css";
 
+window.onload = function () {
+    // Arrays con parlabras
+    let pronombres = ['mi', 'tu', 'el', 'nuestro'];
+    let adjetivos = ['rápido', 'fuerte', 'gran', 'enorme'];
+    let sustantivos = ['león', 'águila', 'arbol', 'silla'];
+    let extensiones = ['.com', '.net', '.io', '.es']
+    let dominiosGenerados = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    // Elementos del DOM
+    const contenedor = document.getElementById('dominios-lista');
+    const boton = document.getElementById('generar-btn')
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    function generarDominio() {
+        const pronombre = pronombres[Math.floor(Math.random() * pronombres.length)];
+        const adjetivo = adjetivos[Math.floor(Math.random() * adjetivos.length)];
+        const sustantivo = sustantivos[Math.floor(Math.random() * sustantivos.length)];
+        const extension = extensiones[Math.floor(Math.random() * extensiones.length)];
+
+        return pronombre + adjetivo + sustantivo + extension;
+    }
+
+    // Evento click
+     boton.addEventListener("click", () => {
+        const nuevoDominio = generarDominio();
+        dominiosGenerados.push(nuevoDominio);
+
+        // Mostrar solo el último dominio en el div (reemplaza el anterior)
+        contenedor.innerHTML = `<h4>${nuevoDominio}</h4>`;
+
+        // Mostrar todos los dominios generados en la consola
+        console.clear();
+        console.log("Dominios generados hasta ahora:");
+        dominiosGenerados.forEach((dominio, index) => {
+            console.log(`${index + 1}: ${dominio}`);
+        });
+    });
 };
